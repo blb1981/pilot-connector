30c6eeb3c5a297b0e46bc5a81e0137b7b95fce15
Brandon <brandonbland90@gmail.com>
2021-12-27 16:20:40 -0500

    Added POST route for jobs.

--- MEAN/test.js
+++
@@ -1,9 +0,0 @@
-// let arr1 = ['hey', 'ho']
-// console.log(arr1)
-
-// let arr2 = ...arr1
-// console.log(arr2)
-
-(async  () => {
-	console.log('testing 123')
-})()
--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,50 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+// const bodyParser = require('body-parser')
+
+app.use(express.json())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded([{ extended: false }]))
 
 // Add the following headers for CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  )
+  next()
+})
 
-app.get("/api/jobs", (req, res) => {
+app.post('/api/jobs', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+app.get('/api/jobs', (req, res) => {
   posts = [
     {
-      id: "123f131fadsf",
-      title: "First post",
+      id: '123f131fadsf',
+      title: 'First post',
       content: "This is the first post's content",
     },
     {
-      id: "123f131fewrr",
-      title: "Second post",
+      id: '123f131fewrr',
+      title: 'Second post',
       content: "This is the second post's content",
     },
     {
-      id: "123f131fzxcv",
-      title: "Third post",
+      id: '123f131fzxcv',
+      title: 'Third post',
       content: "This is the third post's content",
     },
-  ];
+  ]
 
-  res.status(200).json(posts);
-});
+  res.status(200).json(posts)
+})
 
-module.exports = app;
+module.exports = app
