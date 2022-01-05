6ea8d36646866ea993d597a622410aa21d1bc90d
Brandon <brandonbland90@gmail.com>
2022-01-05 12:16:11 -0500

    Added image selection/preview for a job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
@@ -10,7 +10,12 @@ import { JobsListComponent } from './jobs/jobs-list/jobs-list.component'
 
 @NgModule({
   declarations: [AppComponent, JobsCreateComponent, JobsListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/jobs/jobs-create/jobs-create.component.ts
+++ src/app/jobs/jobs-create/jobs-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, ParamMap } from '@angular/router'
-import { NgForm } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 import { JobsService } from '../jobs.service'
 import { Job } from '../job.model'
@@ -8,18 +8,31 @@ import { Job } from '../job.model'
 @Component({
   selector: 'app-jobs-create',
   templateUrl: './jobs-create.component.html',
+  styleUrls: ['./jobs-create.component.css'],
 })
 export class JobsCreateComponent implements OnInit {
   enteredTitle = ''
   enteredContent = ''
   job: Job
   isLoading: boolean = false
+  form: FormGroup
+  imagePreview: string
   mode = 'create'
   private id: string
 
   constructor(public jobsService: JobsService, public route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.min(3)],
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      image: new FormControl(null, { validators: [Validators.required] }),
+    })
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('id')) {
         this.mode = 'edit'
@@ -32,6 +45,10 @@ export class JobsCreateComponent implements OnInit {
             title: response.data.job.title,
             content: response.data.job.content,
           }
+          this.form.setValue({
+            title: this.job.title,
+            content: this.job.content,
+          })
         })
       } else {
         this.mode = 'create'
@@ -40,17 +57,32 @@ export class JobsCreateComponent implements OnInit {
     })
   }
 
-  onSaveJob(form: NgForm) {
-    if (form.invalid) {
+  onSaveJob() {
+    if (this.form.invalid) {
       return
     }
     this.isLoading = true
     if (this.mode === 'create') {
-      this.jobsService.addJob(form.value.title, form.value.content)
+      this.jobsService.addJob(this.form.value.title, this.form.value.content)
     } else {
-      this.jobsService.updatejob(this.id, form.value.title, form.value.content)
+      this.jobsService.updatejob(
+        this.id,
+        this.form.value.title,
+        this.form.value.content
+      )
     }
 
-    form.resetForm()
+    this.form.reset()
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0]
+    this.form.patchValue({ image: file })
+    this.form.get('image').updateValueAndValidity()
+    const reader = new FileReader()
+    reader.onload = () => {
+      this.imagePreview = reader.result as string
+    }
+    reader.readAsDataURL(file)
   }
 }
