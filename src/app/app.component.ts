6c65e59f44c1060464d6171d4d06dc64f59451d2
Brandon <brandonbland90@gmail.com>
2022-01-10 01:09:33 -0500

    Added conditional render of message for no jobs displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Pilot Connector';
+  title = 'Pilot Connector'
   summary = 'Free job postings for pilots'
-
-
-
 }
