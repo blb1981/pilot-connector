d551c25e6116a88f16d60a6c1dde7f05bc3c633e
Brandon <brandonbland90@gmail.com>
2022-01-11 12:45:37 -0500

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { JobsCreateComponent } from './jobs/jobs-create/jobs-create.component'
-import { JobsListComponent } from './jobs/jobs-list/jobs-list.component'
+import { JobsListComponent } from './jobs/jobs-list/jobs-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, JobsCreateComponent, JobsListComponent],
@@ -15,6 +16,7 @@ import { JobsListComponent } from './jobs/jobs-list/jobs-list.component'
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
