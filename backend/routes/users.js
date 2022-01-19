7aa19fb1fd7fb23f136d61103f85aa0cdb768e6d
Brandon <brandonbland90@gmail.com>
2022-01-19 00:07:14 -0500

    Completed jwt implementation and login functionality for backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
@@ -23,6 +24,58 @@ router.post('/register', (req, res) => {
           status: 'fail',
           data: {
             error: err,
+          },
+        })
+      })
+  })
+})
+
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    // If email not found
+    if (!user) {
+      return res.status(401).json({
+        status: 'fail',
+        data: {
+          message: 'Authentication failed',
+        },
+      })
+    }
+
+    // Email found
+    bcrypt
+      .compare(req.body.password, user.password)
+      .then((result) => {
+        // If passwords don't match
+        if (!result) {
+          return res.status(401).json({
+            status: 'fail',
+            data: {
+              message: 'Authentication failed',
+            },
+          })
+        }
+        // Password matches, create JWT
+        const token = jwt.sign(
+          { email: user.email, id: user._id },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+          }
+        )
+        res.status(200).json({
+          status: 'success',
+          data: {
+            token,
+          },
+        })
+      })
+      .catch((err) => {
+        // Some other type of error
+        return res.status(401).json({
+          status: 'fail',
+          data: {
+            message: 'Authentication failed',
           },
         })
       })
