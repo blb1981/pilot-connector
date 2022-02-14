aed3b240f65045d7ddb4a17f10a10411c69f2400
Brandon <brandonbland90@gmail.com>
2022-02-14 00:01:24 -0500

    Added cors middleware package to app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const jobsRoutes = require('./routes/jobs')
 const authRoutes = require('./routes/auth')
@@ -25,19 +26,22 @@ app.use(express.json())
 // Middleware for image requests
 app.use('/images', express.static(path.join('backend/images')))
 
+// Middleware for CORS
+app.use(cors())
+
 // Headers for CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  )
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+//   )
+//   next()
+// })
 
 app.use('/api/jobs', jobsRoutes)
 app.use('/api/auth', authRoutes)
